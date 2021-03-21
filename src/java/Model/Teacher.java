2a38b592e92dc25e5206299b617cd104e4b4a478
PhuongNguyen <nguyenlongphuong2000@gmail.com>
2021-03-21 22:42:20 +0700

    ssss

--- src/java/Model/Teacher.java
+++ src/java/Model/Teacher.java
@@ -20,5 +20,27 @@ public class Teacher {
         this.name = name;
         this.desc = desc;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" + "name=" + name + ", desc=" + desc + '}';
+    }
+    
     
 }
